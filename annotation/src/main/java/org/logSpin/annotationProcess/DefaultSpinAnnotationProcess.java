c362135e46ca9a190e27ee37d097d3307e7e3fa1
“Tian,Xun” <xtian@telenavsoftware.com>
2020-11-04 18:43:53 +0800

    Add apply external plugin phase 3

--- annotation/src/main/java/org/logSpin/annotationProcess/DefaultSpinAnnotationProcess.java
+++ annotation/src/main/java/org/logSpin/annotationProcess/DefaultSpinAnnotationProcess.java
@@ -7,6 +7,8 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -21,11 +23,25 @@ public class DefaultSpinAnnotationProcess extends AbstractProcessor {
         }
 
         List<Element> classList = new ArrayList<>(roundEnv.getElementsAnnotatedWith(org.logSpin.annotation.Plugin.class));
-        classList.forEach(element -> {
-            System.out.println("process ------------"+element.getSimpleName());
-
-        });
+        write(classList);
         return false;
+    }
+
+    private void write(List<Element> classList) {
+        String fileName = "Plugins.txt";
+        try {
+            FileWriter writer = new FileWriter(fileName);
+            classList.forEach(element -> {
+                try {
+                    writer.write(((TypeElement)element).getQualifiedName()+"\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
